71cae465c9e019b01abefd453660a21818c29b9a
JamesArbogast <jamesarbogast52@gmail.com>
2021-06-29 21:48:26 -0700

    Working Language Converter

--- bot.py
+++ bot.py
@@ -1,15 +1,33 @@
-# bot.py
 import os
-
 import discord
+import pyperclip
 
-TOKEN = 'ODU5MjAzNjU0Mzc2NzUxMTM2.YNpRwg.LGunq2GxFzItIf-SSLqGyYI-EqA'
-print(TOKEN)
+my_secret = os.environ['bot']
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print('We have logged in as {0.user}'.format(client))
 
-client.run(TOKEN)
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if str(message.channel) == "general" and message.content.startswith('/'):
+        await message.channel.purge(limit=1)
+        if message.content.startswith('/html'):
+            content = message.content.replace('/html', '')
+            nl = '\n'
+            await message.channel.send(f'```html{nl}{content}{nl}```')
+            print("This is working HTML")
+        if message.content.startswith('/py'):
+            content = message.content.replace('/py', '')
+            nl = '\n'
+            await message.channel.send(f'```py{nl}{content}{nl}```')
+            print("This is working for python")
+        if message.content.startswith('/js'):
+            content = message.content.replace('/js', '')
+            nl = '\n'
+            await message.channel.send(f'```js{nl}{content}{nl}```')
+            print("This is working for JS")
